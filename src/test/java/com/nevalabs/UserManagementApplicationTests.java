1fb69647eb608afac4835c32ec6386a08da8c680
serpilkuzu <serpil.kuzu@nevalabs.com>
2017-02-03 18:02:53 +0300

    us-3 Created unit tests for searching method to filter user and create user

--- src/test/java/com/nevalabs/UserManagementApplicationTests.java
+++ src/test/java/com/nevalabs/UserManagementApplicationTests.java
@@ -1,16 +1,55 @@
 package com.nevalabs;
 
+import com.nevalabs.model.User;
+import com.nevalabs.repositories.UserRepository;
+import com.nevalabs.service.UserService;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserManagementApplicationTests {
 
+    @Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	UserService userService;
+
+	private static int userId;
+	private static String name = "test name";
+	private static String surname = "test surname";
+
+	@Before
+	public void setUpOnce() {
+        User testUser = new User();
+        testUser.setName(name);
+        testUser.setSurname(surname);
+        userId = userRepository.save(testUser).getId();
+    }
+
 	@Test
-	public void contextLoads() {
+	public void testSearchByNameAndSurname(){
+		List<User> userList = userService.searchByNameAndSurname(name.substring(3, name.length()-2), surname.substring(2));
+        assert userList.stream().filter(user -> user.getId() == userId).findAny().isPresent();
 	}
+
+	@Test
+    public void testCreateUser() {
+	    User user = userRepository.findOne(userId);
+	    assert user.getName().equals(name);
+    }
+
+    @After
+    public void deleteUser() {
+	    userRepository.delete(userId);
+    }
 
 }
